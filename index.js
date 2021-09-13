c14a43973f3a6f80c1e6146e6f154b076f0ef386
pavlvs <pavlvsxavier@gmail.com>
2021-09-13 12:34:58 +0100

    added form and event listener

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hiya')
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const input = document.querySelector('input')
+    console.log(input.value)
+})
